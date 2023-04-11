6f33d111776f07b6df9897279ad7bc8cad82dd66
anton2134 <101354461+anton2134@users.noreply.github.com>
2023-04-11 23:54:34 +0700

    Delete dbo.udf_GetSKUPrice.sql.sql

--- dbo.udf_GetSKUPrice.sql.sql
+++
@@ -1,9 +0,0 @@
-create function dbo.udf_GetSKUPrice (@ID_SKU int) returns decimal(18,2)
-as 
-begin
-declare @cost decimal;
-select @cost = sum(dbo.Basket.Value)/sum(dbo.Basket.Quantity)
-from dbo.Basket
-where dbo.Basket.ID_SKU = @ID_SKU;
-return @cost
-end;
